188bc4bae320d83be97bdb5a2cc1a98e81901cb3
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-11 21:49:23 +0200

    feat: table in trains page

--- resources/views/layouts/trains.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1 class="text-center">Trains</h1>
-@endsection
---
+++ resources/views/trains.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-center">Trains</h1>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+        </tbody>
+    </table>
+@endsection
