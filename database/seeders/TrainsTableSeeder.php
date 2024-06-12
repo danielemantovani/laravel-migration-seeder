b12c51ab18733b9eea4347acf2a5e4955c5d9e23
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-12 18:25:28 +0200

    feat: faker data

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Faker;
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,19 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker\Factory::create('it_IT');
+        for ($i = 0; $i < 20; $i++) {
+            $newTrain = new Train();
+            $newTrain->company = $faker->randomElements(['a', 'b', 'c']);
+            $newTrain->departureStation = $faker->city();
+            $newTrain->arrivalStation = $faker->city();
+            $newTrain->departureTime = $faker->time();
+            $newTrain->arrivalTime = $faker->time();
+            $newTrain->trainNumber = $faker->numberBetween(0, 99);
+            $newTrain->carriagesNumber = $faker->numberBetween(0, 99);
+            $newTrain->inTime = $faker->boolean();
+            $newTrain->cencelled = $faker->boolean();
+            $newTrain->save();
+        }
     }
 }
